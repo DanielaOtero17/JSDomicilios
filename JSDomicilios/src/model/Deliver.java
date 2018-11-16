4ca61cbf154090b82181808fc42ce8dc6fde655a
jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2018-11-16 08:06:43 -0500

    implements class deliever and products

--- JSDomicilios/src/model/Deliver.java
+++ JSDomicilios/src/model/Deliver.java
@@ -2,6 +2,35 @@ package model;
 
 public class Deliver {
 	
-	// is in progress
+	private String client;
+	private int[] address;
+	
+	public Deliver(String client, int[] address) {
+		this.client = client;
+		this.address = address;
+	}
+
+	public String getClient() {
+		return client;
+	}
+
+	public void setClient(String client) {
+		this.client = client;
+	}
+
+	public int[] getAddress() {
+		return address;
+	}
+
+	public void setAddress(int[] address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return  client;
+	}
+	
+	
 
 }
--- JSDomicilios/src/model/Product.java
+++ JSDomicilios/src/model/Product.java
@@ -2,6 +2,28 @@ package model;
 
 public class Product {
 	
-	// is in progress
-
+	private String name;
+	private double value;
+	
+	public Product(String nombre, double valor) {
+		this.name = nombre;
+		this.value = valor;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	public String getNombre() {
+		return name;
+	}
+	public void setNombre(String nombre) {
+		this.name = nombre;
+	}
+	public double getValor() {
+		return value;
+	}
+	public void setValor(double valor) {
+		this.value = valor;
+	}
 }
