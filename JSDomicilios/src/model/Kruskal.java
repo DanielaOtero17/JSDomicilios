5b757d2031a33d9f89fa63786083de753c4cdd6a
DanielaOtero17 <jessot@hotmail.com>
2018-11-26 14:25:51 -0500

    kruskal class is being added.

--- JSDomicilios/src/model/Kruskal.java
+++ JSDomicilios/src/model/Kruskal.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Kruskal {
+import java.util.*;
 
+
+public class Kruskal<E,T>{
+
+	public boolean HayCiclo(Graph<E,T> g,Edge<E,T> aVerificar,Vertex<E,T> terminal,String N)
+	 {
+	 	ArrayList<Vertex<E,T>> aux= new ArrayList<>();
+	 	
+	 	for(int i =0; i<terminal.getNeighbors().length;i++){
+	 		
+	 		aux.add(terminal.getNeighbors()[i]);
+	 	}
+	 
+	 	if(aux.size()==0)
+	 		return false;
+	 
+	 	if(terminal.existeEnlace(aVerificar.getPartida())!=-1)
+	 		return true;
+	 
+	 	for(int i=0;i<aux.size();i++)
+	 	{
+	 		Enlace nodo=aux.get(i);
+	 
+	 		if(nodo.getDestino().equals(N)==false)
+	 			if( HayCiclo(g,aVerificar,g.getNodo(nodo.getDestino()),terminal.getIndex()))
+	 								return true;
+	 	}
+	 
+	 	return false;
+	 }
+	
 }
