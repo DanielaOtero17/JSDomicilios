a769ffe7a3f0ae13b54bf8aa54718fe471cba48a
jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2018-11-26 17:46:45 -0500

    kruskal

--- JSDomicilios/src/model/Kruskal.java
+++ JSDomicilios/src/model/Kruskal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 import java.util.PriorityQueue;
@@ -46,40 +45,3 @@ public class Kruskal<E,T> extends ArbolRecubridorMinimo<E,T> {
     }
 
 }
-=======
-package model;
-
-import java.util.*;
-
-
-public class Kruskal<E,T>{
-
-	public boolean HayCiclo(Graph<E,T> g,Edge<E,T> aVerificar,Vertex<E,T> terminal,String N)
-	 {
-	 	ArrayList<Vertex<E,T>> aux= new ArrayList<>();
-	 	
-	 	for(int i =0; i<terminal.getNeighbors().length;i++){
-	 		
-	 		aux.add(terminal.getNeighbors()[i]);
-	 	}
-	 
-	 	if(aux.size()==0)
-	 		return false;
-	 
-	 	if(terminal.existeEnlace(aVerificar.getPartida())!=-1)
-	 		return true;
-	 
-	 	for(int i=0;i<aux.size();i++)
-	 	{
-	 		Enlace nodo=aux.get(i);
-	 
-	 		if(nodo.getDestino().equals(N)==false)
-	 			if( HayCiclo(g,aVerificar,g.getNodo(nodo.getDestino()),terminal.getIndex()))
-	 								return true;
-	 	}
-	 
-	 	return false;
-	 }
-	
-}
->>>>>>> branch 'master' of https://github.com/DanielaOtero17/JSDomicilios.git
